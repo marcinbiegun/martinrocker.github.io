498737aaaf9feb9bdec17955c207ceec7076dbdf
Marcin Biegun <marcin@mm.st>
2022-03-02 16:27:37 +0100

    Build site to /docs directory

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Hello world 2!",
+        "excerpt":"This is my first post.  ","categories": ["hello"],
+        "tags": [],
+        "url": "/hello-world-2/",
+        "teaser": null
+      },{
+        "title": "Hello world 3!",
+        "excerpt":"This is my first post.  ","categories": ["hello"],
+        "tags": [],
+        "url": "/hello-world-3/",
+        "teaser": null
+      },{
+        "title": "Hello world 4!",
+        "excerpt":"This is my first post.  ","categories": ["hello"],
+        "tags": [],
+        "url": "/hello-world-4/",
+        "teaser": null
+      },{
+        "title": "Hello world 4!",
+        "excerpt":"This is my first post.  ","categories": ["hello"],
+        "tags": [],
+        "url": "/hello-world-5/",
+        "teaser": null
+      },{
+        "title": "Hello world!",
+        "excerpt":"This is my first post.  ","categories": ["hello"],
+        "tags": [],
+        "url": "/hello-world/",
+        "teaser": null
+      }]
